f014369fb5d5774454c1fd0f8fb237d2e11d883b
zhiyuan.liang <zhiyuan_liang@westwell-lab.com>
2025-08-07 11:18:46 +0800

    update

--- cmd/chaos/cmd/fms/vehicle_status.go
+++ cmd/chaos/cmd/fms/vehicle_status.go
@@ -309,7 +309,7 @@ func printVehicles(ctx context.Context, vehicles fms.Vehicles) {
 		var waitColor text.Colors
 		if vehicle.KaTime == 0 || vehicle.CallTime == 0 {
 			waitTime = ""
-		} else if vehicle.KaTime > 0 && vehicle.CallTime == 0 {
+		} else if vehicle.KaTime > 0 && vehicle.CurrentDestination.Type != "CALLIN_AREA" {
 			dt := time.Now().UnixMilli() - int64(vehicle.KaTime)
 			if 120 <= dt && dt < 300 {
 				waitColor = text.Colors{text.BgGreen}
